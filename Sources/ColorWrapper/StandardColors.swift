bbde387a65a49b6953e2c75df67f8f97e51142c7
Brian Majewski <brian.majewski@gmail.com>
2024-03-06 09:04:36 -0800

    Added in Standard Colors

---
+++ Sources/ColorWrapper/StandardColors.swift
@@ -0,0 +1,124 @@
+//
+//  StandardColors.swift
+//  ColorWrapper
+//  Wrapes Standard Colors from UIKit into SwiftUI Color Elements
+//  see: https://developer.apple.com/documentation/uikit/uicolor/standard_colors
+//  Swatches at: https://developer.apple.com/design/human-interface-guidelines/color#system-colors
+//  Created by Brian Majewski on 3/6/24.
+//
+
+import SwiftUI
+
+
+public class StandardColors {
+    
+    // Adaptable Colors
+    
+    /// a blue color that automatically adapts to the current trait environment
+    public static let systemBlue: Color = Color(uiColor: UIColor.systemBlue)
+    
+    /// a brown color that automatically adapts to the current trait environment
+    public static let systemBrown: Color = Color(uiColor: UIColor.systemBrown)
+     
+    /// a cyan color that automatically adapts to the current trait environment
+    public static let systemCyan: Color = Color(uiColor: UIColor.systemCyan)
+     
+    /// a green color that automatically adapts to the current trait environment
+    public static let systemGreen: Color = Color(uiColor: UIColor.systemGreen)
+     
+    /// an indigo color that automatically adapts to the current trait environment
+    public static let systemIndigo: Color = Color(uiColor: UIColor.systemIndigo)
+     
+    /// a mint color that automatically adapts to the current trait environment
+    public static let systemMint: Color = Color(uiColor: UIColor.systemMint)
+     
+    /// an orange color that automatically adapts to the current trait environment
+    public static let systemOrange: Color = Color(uiColor: UIColor.systemOrange)
+     
+    /// a pink color that automatically adapts to the current trait environment
+    public static let systemPink: Color = Color(uiColor: UIColor.systemPink)
+     
+    /// a purple color that automatically adapts to the current trait environment
+    public static let systemPurple: Color = Color(uiColor: UIColor.systemPurple)
+    
+    /// a red color that automatically adapts to the current trait environment
+    public static let systemRed: Color = Color(uiColor: UIColor.systemRed)
+     
+    /// a teal color that automatically adapts to the current trait environment
+    public static let systemTeal: Color = Color(uiColor: UIColor.systemTeal)
+     
+    /// a yellow color that automatically adapts to the current trait environment
+    public static let systemYellow: Color = Color(uiColor: UIColor.systemYellow)
+    
+    // Adaptable gray colors
+    
+    /// the standard base gray that automatically adapts to the environment
+    public static let systemGray: Color = Color(uiColor: UIColor.systemGray)
+    
+    /// the standard base gray that automatically adapts to the environment
+    public static let systemGray2: Color = Color(uiColor: UIColor.systemGray2)
+    
+    /// the standard base gray that automatically adapts to the environment
+    public static let systemGray3: Color = Color(uiColor: UIColor.systemGray3)
+    
+    /// the standard base gray that automatically adapts to the environment
+    public static let systemGray4: Color = Color(uiColor: UIColor.systemGray4)
+    
+    /// the standard base gray that automatically adapts to the environment
+    public static let systemGray5: Color = Color(uiColor: UIColor.systemGray5)
+    
+    /// the standard base gray that automatically adapts to the environment
+    public static let systemGray6: Color = Color(uiColor: UIColor.systemGray6)
+    
+
+    // Transparent Color
+    
+    /// a color object with grayscale and alpha values that are both 0.0
+    public static let clear: Color = Color(uiColor: UIColor.clear)
+    
+    // Fixed Colors
+    
+    /// A color object in the sRGB color space with a grayscale value of 0.0 and an alpha value of 1.0.
+    public static let black: Color = Color(uiColor: UIColor.black)
+    
+    /// A color object with RGB values of 0.0, 0.0, and 1.0, and an alpha value of 1.0.
+    public static let blue: Color = Color(uiColor: UIColor.blue)
+    
+    /// A color object with RGB values of 0.6, 0.4, and 0.2, and an alpha value of 1.0.
+    public static let brown: Color = Color(uiColor: UIColor.brown)
+    
+    /// A color object with RGB values of 0.0, 1.0, and 1.0, and an alpha value of 1.0.
+    public static let cyan: Color = Color(uiColor: UIColor.cyan)
+    
+    /// A color object with a grayscale value of 1/3 and an alpha value of 1.0.
+    public static let darkGray: Color = Color(uiColor: UIColor.darkGray)
+    
+    /// A color object with a grayscale value of 0.5 and an alpha value of 1.0.
+    public static let gray: Color = Color(uiColor: UIColor.gray)
+    
+    /// A color object with RGB values of 0.0, 1.0, and 0.0, and an alpha value of 1.0.
+    public static let green: Color = Color(uiColor: UIColor.green)
+    
+    /// A color object with a grayscale value of 2/3 and an alpha value of 1.0.
+    public static let lightGray: Color = Color(uiColor: UIColor.lightGray)
+    
+    /// A color object with RGB values of 1.0, 0.0, and 1.0, and an alpha value of 1.0.
+    public static let magenta: Color = Color(uiColor: UIColor.magenta)
+    
+    /// A color object with RGB values of 1.0, 0.5, and 0.0, and an alpha value of 1.0.
+    public static let orange: Color = Color(uiColor: UIColor.orange)
+    
+    /// A color object with RGB values of 0.5, 0.0, and 0.5, and an alpha value of 1.0.
+    public static let purple: Color = Color(uiColor: UIColor.purple)
+    
+    /// A color object with RGB values of 1.0, 0.0, and 0.0, and an alpha value of 1.0.
+    public static let red: Color = Color(uiColor: UIColor.red)
+    
+    /// A color object with a grayscale value of 1.0 and an alpha value of 1.0.
+    public static let white: Color = Color(uiColor: UIColor.white)
+    
+    /// A color object with RGB values of 1.0, 1.0, and 0.0, and an alpha value of 1.0.
+    public static let yellow: Color = Color(uiColor: UIColor.yellow)
+    
+    
+}
